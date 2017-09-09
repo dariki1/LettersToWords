e4895f9bbb44989a28ba4e51e2550b8c94c0d357
dariki1 <james_atkins97@hotmail.com>
2017-09-09 18:21:14 +1000

    Working with hard coded dictionary
    Program takes letters in and spits out all possible word combinations
    with hard coded dictionary

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LettersToWords {
+	class Program {
+
+		static void Main(string[] args) {
+			WordHandler.addWord("POTATO");
+			WordHandler.addWord("POTATOES");
+			WordHandler.addWord("POT");
+			WordHandler.addWord("TAO");
+			Console.WriteLine("Please enter a set of letters");
+			List<String> w = WordHandler.getWords(Console.ReadLine());
+			foreach (String word in w) {
+				Console.WriteLine(word);
+			}				
+			Console.ReadKey();
+		}
+	}
+}
