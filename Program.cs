8615cf4afc4cd49261f0e012c405644dc948bf1b
dariki1 <james_atkins97@hotmail.com>
2017-09-09 18:38:15 +1000

    Added dictionary and file reader
    Added dictionary (https://github.com/dwyl/english-words, removed single
    letters) and file reader

--- Program.cs
+++ Program.cs
@@ -8,16 +8,17 @@ namespace LettersToWords {
 	class Program {
 
 		static void Main(string[] args) {
-			WordHandler.addWord("POTATO");
-			WordHandler.addWord("POTATOES");
-			WordHandler.addWord("POT");
-			WordHandler.addWord("TAO");
-			Console.WriteLine("Please enter a set of letters");
-			List<String> w = WordHandler.getWords(Console.ReadLine());
-			foreach (String word in w) {
-				Console.WriteLine(word);
-			}				
-			Console.ReadKey();
+			String[] wordList = FileHandler.readFile();
+			foreach (String s in wordList) {
+				WordHandler.addWord(s);
+			}
+			do {
+				Console.WriteLine("Please enter a set of letters");
+				List<String> w = WordHandler.getWords(Console.ReadLine());
+				foreach (String word in w) {
+					Console.WriteLine(word);
+				}
+			} while (true);
 		}
 	}
 }
