a403e6659497d64dd1cc653fa19c4970e55a7d39
dariki1 <james_atkins97@hotmail.com>
2017-09-24 23:36:01 +1000

    Moved files to push solution file
    I set up the local repository wrong, and it didn't push the .sln file,
    no code changes

---
+++ LettersToWords/CharacterNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LettersToWords {
+	class CharacterNode {
+		public readonly char myChar;
+		public List<CharacterNode> nextNodes = new List<CharacterNode>();
+		public bool isWord = false;
+
+		public CharacterNode(char mChar) {
+			myChar = mChar;
+		}
+	}
+}
